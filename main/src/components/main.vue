b1c0d219bca7d7bcbfc9826227028c7f4077e99a
shravan1998 <shrkum51@gmail.com>
2019-01-26 19:17:34 +0530

    installed libraries

---
+++ main/src/components/main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>DevHost</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:Main
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1 {
+  margin: 40px 0 0;
+  text-align: left;
+  color:white
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+body {
+  background-color: black
+}
+</style>
---
+++ main/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>DevHost</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+   
+  },
+  data() {
+    return{
+      
+    }
+  } 
+}
+</script>
+
+<style scoped>
+
+</style>
+
