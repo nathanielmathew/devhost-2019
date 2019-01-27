afe1d916836349206a81b8826f75446c7f4069cf
shravan1998 <shrkum51@gmail.com>
2019-01-27 16:04:59 +0530

    router links added

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-     
+      DevHost
+      <ul id="nav-links" style="list-style-type:none">
+        <li ><router-link to="/">Home</router-link></li>
+        <li><router-link to="/events">Events</router-link></li>
+        <li><router-link to="/sponsors">Sponsors</router-link></li>
+        <li><router-link to="/about">About Us</router-link></li>
+      </ul>
     </div>
     <router-view/>
   </div>
@@ -12,8 +18,20 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align: left;
+  color: white;
+ /* margin-top: 30px; */
+  font-size: 40px;
+  background-color: #080331;
+}
+#nav-links a{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: right;
+  color: white;
+  margin-top: 30px;
+  font-size: 20px;
+  background-color: #2c3e50;
 }
 </style>
--- main/src/views/Home.vue
+++ main/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>DevHost</h1>
+    
   </div>
 </template>
 
